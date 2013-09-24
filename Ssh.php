90bcc56e3c388032a7fa26fbcac24d31ec5ded3b
xternalx <i@xternalx.com>
2013-09-24 20:10:36 +0800

    Shell mode improvements. Now User command promt is not shown, multiline scripts not shown. Shell mode now outpust only commands ouptup, executed in shell mode

--- Ssh.php
+++ Ssh.php
@@ -3,15 +3,19 @@
 class Ssh
 {
 
-    private $conn;
-    private $err;
-    private $out;
-    private $in;
-    
-    private $shell;
-    
+    private $conn = null;
+    private $err = null;
+    private $out = null;
+    private $in = null;
+    private $shell = null;
     private $host;
     private $port;
+    private $eventHandlers = array();
+    private $lastCmd = null;
+    private $shellCommandPrompt = null;
+
+    const EVENT_ON_COMMAND_COMPLETE = 0;
+    const EVENT_ON_SHELL_COMPLETE = 1;
 
     public function __construct()
     {
@@ -20,12 +24,12 @@ class Ssh
             throw new Exception("ssh2 module not installed!");
         }
     }
-    
+
     public function __call($name, $arguments)
     {
         if (!in_array($name, get_class_methods($this)))
         {
-            return $this->exec($name." ".implode(" ", $arguments));
+            return $this->exec($name . " " . implode(" ", $arguments));
         }
     }
 
@@ -33,82 +37,154 @@ class Ssh
     {
         $this->host = $host;
         $this->port = $port;
-        
-        $this->conn = ssh2_connect($host, $port);
-        if ($this->conn === false)
+
+        $this->conn = @ssh2_connect($host, $port);
+        if (!$this->conn)
         {
             throw new Exception("Failed to connect to host {$host}:{$port}");
         }
+    }
+
+    public function disconnect()
+    {
+        $this->conn = null;
+        unset($this->conn);
     }
 
     public function auth($login, $passwd)
     {
         if (!ssh2_auth_password($this->conn, $login, $passwd))
         {
-            throw new Exception("Authentication failed!");
+            throw new Exception("Authentication with login and password failed!");
         }
     }
-    
+
+    public function authFile($login, $pubkey, $privkey = null, $pass = null)
+    {
+        if (!ssh2_auth_pubkey_file($this->conn, $login, $pubkey, $privkey, $pass))
+        {
+            throw new Exception("Authorization with public key failed");
+        }
+    }
+
+    public function getExitCode()
+    {
+        return intval($this->exec('echo $?'));
+    }
+
     public function exec($cmd)
     {
-        $this->out = ssh2_exec($this->conn, $cmd);
-            
-        $this->error = ssh2_fetch_stream($this->out, SSH2_STREAM_STDERR);
-        $errorMessage = $this->readStream($this->error);
-        if (strlen($errorMessage) != 0)
+        // removing blank lines or comments
+        $cmd = preg_replace("/(^[\r\n\#]*|[\r\n]+)[\s\t]*[\r\n]+/", "", $cmd);
+        $this->lastCmd = $cmd;
+        if ($this->shell !== null)
         {
-            throw new Exception($errorMessage);
+            return $this->shell($cmd);
         }
+        else
+        {
+            $stream = ssh2_exec($this->conn, $cmd);
 
-        return $this->readStream($this->out);
+            $this->error = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
+            $errorMessage = $this->readStream($this->error, true);
+            if (strlen($errorMessage) != 0)
+            {
+                throw new Exception($errorMessage);
+            }
+
+            return $this->readStream($stream);
+        }
     }
-    
+
+    public function execBatch($code, $delimiter = "\r\n")
+    {
+        $commands = explode("\r\n", $code);
+        foreach ($commands as $cmd)
+        {
+            $result = $this->exec($cmd);
+        }
+        return $result;
+    }
+
     public function shell($cmd)
     {
-        if($this->shell == null)
+        $this->lastCmd = $cmd;
+        if ($this->shell == null)
         {
             $this->shell = ssh2_shell($this->conn);
-            sleep(1);
+            //sleep(1);
+            $welcome = explode("\n", $this->readShellOutput());
+            $this->shellCommandPrompt = array_pop($welcome);
         }
-        
-        $this->readShellOutput();
-        
-        fwrite($this->shell, $cmd.PHP_EOL);
-        sleep(1);
-        
+
+        fwrite($this->shell, $cmd . PHP_EOL);
+
         return $this->readShellOutput();
-        
     }
-    
+
     public function endShell()
     {
         fclose($this->shell);
-    }
-    
-    protected function readStream($stream)
-    {
-        $response = array();
-        stream_set_blocking($stream, true);
-        while ($response[] = fgets($stream))
-        {
-            flush();
-        }
-        stream_set_blocking($stream, false);
-        return implode("", $response);
+        $this->shell = null;
     }
 
-    protected function readShellOutput()
+    protected function readStream($stream, $isStdErr = false)
     {
-        $out = '';
-        while ( $line = fgets ( $this->shell ) ) {
-            $out .= $line;
-        }
-        return $out;
+        stream_set_blocking($stream, true);
+        $response = stream_get_contents($stream);
+        stream_set_blocking($stream, false);
+        if (!$isStdErr)
+            $this->raiseEvent(self::EVENT_ON_COMMAND_COMPLETE, array($this->lastCmd, $response));
+        return $response;
     }
-    
-    public function disconnect()
+
+    protected function readShellOutput($raiseEvent = true)
     {
-        
+        $response = null;
+        sleep(1);
+        if ($this->shellCommandPrompt == null)
+        {
+            $response = stream_get_contents($this->shell);
+        }
+        else
+        {
+            $response = array();
+            $promptLen = strlen($this->shellCommandPrompt);
+            while ($line = fgets($this->shell))
+            {
+                if (substr($line, 0, $promptLen) == $this->shellCommandPrompt)
+                {
+                    
+                } // do nothing
+                else if (substr($line, 0, 2) == "> ")
+                {
+                    
+                } // do nothing
+                else
+                {
+                    $response[] = $line;
+                }
+            }
+            $response = implode("", $response);
+        }
+        if ($raiseEvent)
+            $this->raiseEvent(self::EVENT_ON_SHELL_COMPLETE, array($this->lastCmd, $response));
+        return preg_replace('/^.+\n/', '', $response);
+    }
+
+    public function raiseEvent($event, $args)
+    {
+        if (empty($this->eventHandlers))
+            return;
+        foreach ($this->eventHandlers[$event] as $handler)
+        {
+            call_user_func_array($handler, $args);
+        }
+    }
+
+    public function attachEventHandler($event, $handler)
+    {
+        $this->eventHandlers[$event][] = $handler;
     }
 
 }
